9747bfbfc816e692e7844b14f8d7388771e47d3f
Sean <sean@grieve.com.au>
2013-10-14 14:06:51 +0800

    first

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 var app = {
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
 
     findByName: function() {
         console.log('findByName');
@@ -14,7 +21,10 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+        var self = this;
+        this.store = new MemoryStore(function() {
+            self.showAlert('Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
